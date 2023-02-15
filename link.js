2904fd8b2e5c2029d6bed4ce703eea0c6d1f1cb1
dipuakhando2001 <121944271+dipuakhando2001@users.noreply.github.com>
2023-02-15 16:18:51 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=testtoken=https://test.link.com&html_redirect=1";
+const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=";
