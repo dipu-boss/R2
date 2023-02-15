9503e3204e74d2b864af54cdc14cb679483cb5bf
dipuakhando2001 <121944271+dipuakhando2001@users.noreply.github.com>
2023-02-16 02:17:19 +0600

    Update setlink.js

--- setlink.js
+++ setlink.js
@@ -10,14 +10,14 @@ console.log("....................................................");
 console.log("       This Tool Present by Dipu Ahmed To Any Update Whatsapp 01312701392");
 console.log("````````````````````````````````````````````````````");
 console.log("");
-userInput.question("Enter your link Her Without https://www. Ok ", (value) => {
+userInput.question("Enter your link Her Without https://", (value) => {
   console.log(value);
   fs.writeFile(
     "link.js",
-    `const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqbkRxOHVpc2cwa3BFQXVieE5OMlpYTHRTS1RYZ3xBQ3Jtc0ttQy1EMXVvNHRGSGR0SEJscGhVNmh1ajNyRHF4OVRQYnhPXzljZFBZU0VaVC15X3hTNEN6SmU5RFMyM0JWS2pSYzFBYmhPdm5RQXFGbGdTZzV6OTZPZHg1aW91SWhXVFd3WFh3a1ZFYy11U1VLZUJrRQ&q=${value}&html_redirect=1";`,
+    `const offerLink = "vnd.youtube://${value}&html_redirect=1";`,
     function (err) {
-      console.log("Link set seccussful.");
- 
+      console.log("Link set seccussful. Done And past (netlify deploy --prod)");
+
       process.exit();
     }
   );
