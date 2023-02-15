f079cf6f0fae1ef7371df0a9afebec107ba6b52e
dipuakhando2001 <121944271+dipuakhando2001@users.noreply.github.com>
2023-02-16 01:11:24 +0600

    Update setlink.js

--- setlink.js
+++ setlink.js
@@ -10,11 +10,11 @@ console.log("....................................................");
 console.log("       This Tool Present by Dipu Ahmed");
 console.log("````````````````````````````````````````````````````");
 console.log("");
-userInput.question("Enter your link Her Without https://www. এটা ছারা লিংক দিন:~$ ", (value) => {
+userInput.question("Enter your link Her Without https://www. Ok ", (value) => {
   console.log(value);
   fs.writeFile(
     "link.js",
-    `const offerLink = "vnd.youtube://www.${value}&html_redirect=1";`,
+    `const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqbkRxOHVpc2cwa3BFQXVieE5OMlpYTHRTS1RYZ3xBQ3Jtc0ttQy1EMXVvNHRGSGR0SEJscGhVNmh1ajNyRHF4OVRQYnhPXzljZFBZU0VaVC15X3hTNEN6SmU5RFMyM0JWS2pSYzFBYmhPdm5RQXFGbGdTZzV6OTZPZHg1aW91SWhXVFd3WFh3a1ZFYy11U1VLZUJrRQ&q=${value}&html_redirect=1";`,
     function (err) {
       console.log("Link set seccussful.");
  
