f88c4ba2a9bfe8e232f1128399dc1a9af09d2d87
dipuakhando2001 <121944271+dipuakhando2001@users.noreply.github.com>
2023-02-15 17:15:37 +0600

    Update setlink.js

--- setlink.js
+++ setlink.js
@@ -14,7 +14,7 @@ userInput.question("Enter your link:~$ ", (value) => {
   console.log(value);
   fs.writeFile(
     "link.js",
-    `const offerLink = "vnd.youtube://www.youtube.cvnd.youtube://www.youtube.com/redirect?event=comments&redir_token=${ytLink}=${value}&html_redirect=1";`,
+    `const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=${ytLink}=${value}&html_redirect=1";`,
     function (err) {
       console.log("Link set seccussful.");
  
