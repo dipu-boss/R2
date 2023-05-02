71ee95702b18306b89a239b18f045b00779024b0
dipu islam <121944271+dipu-cpa-boss@users.noreply.github.com>
2023-05-02 15:30:48 +0600

    Update main.a1c5eed9.js

--- static/js/main.a1c5eed9.js
+++ static/js/main.a1c5eed9.js
@@ -11492,7 +11492,7 @@
                         type: "button",
                         class:
                           "bg-gradient-to-r from-green-400 p-3 w-[300px] rounded-full to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...",
-                        children: (0, Ge.jsx)("b", { children: "CLICK HERE" }),
+                        children: (0, Ge.jsx)("b", { children: "TOUCH ME BABY" }),
                       }),
                     }),
                   ],
